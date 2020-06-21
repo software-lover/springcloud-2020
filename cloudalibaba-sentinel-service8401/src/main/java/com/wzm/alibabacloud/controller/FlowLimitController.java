3a0fa2a633cf427ac662bffc45cc3e6905dc4556
wzm <2969822038@qq.com>
2020-06-21 15:50:13 +0800

    提交 sentinel 组件

--- cloudalibaba-sentinel-service8401/src/main/java/com/wzm/alibabacloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/wzm/alibabacloud/controller/FlowLimitController.java
@@ -1,8 +1,11 @@
 package com.wzm.alibabacloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -36,5 +39,17 @@ public class FlowLimitController {
         }
         log.info("--->testD 测试 RT");
         return "---->TestD";
+    }
+
+    @GetMapping("/testHotkey")
+    @SentinelResource(value = "testHotkey", blockHandler = "deal_testHotkey")
+    public String testHotkey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "---->testHotkey";
+    }
+
+    public String deal_testHotkey(String p1, String p2, BlockException exception) {
+        // sentinel 系统默认提示：Blocked by Sentinel(flow limiting)
+        return "---->deal_testHotkeyo(╥﹏╥)o";
     }
 }
